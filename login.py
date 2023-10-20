501d93039d36537091a664539532a21370225057
StraightGG <ss2uang.good@gmail.com>
2023-10-20 16:50:09 +0900

    beta

---
+++ login.py
@@ -0,0 +1,11 @@
+from pyrebase import pyrebase
+import tkinter
+import customtkinter
+
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
+
+app = customtkinter.CTk()
+app.geometry("400x240")
+
+app.mainloop()
--- main.py
+++ main.py
@@ -1,18 +1,42 @@
+﻿import pygame
 import sys
-import pygame
-from pygame.locals import QUIT
+from pygame.locals import *
 
 
 pygame.init()
+
 DISPLAYSURF = pygame.display.set_mode((1280, 720))
 logo = pygame.image.load('images/logo.png')
 pygame.display.set_icon(logo)
 pygame.display.set_caption('Garlic Phone')
+
+screen = pygame.display.set_mode((1280, 720))
+GAME_FONT = pygame.freetype.Font("fonts/ProximaNova.ttf", 24)
+running =  True
+
+logowt = pygame.image.load("images/logowt.png")
+logowt = pygame.transform.scale(logowt, (300, 800))
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.blit(logowt, (100, 100))
+    screen.fill((255,255,255))
+    # You can use `render` and then blit the text surface ...
+    text_surface, rect = GAME_FONT.render("Siwoo Company 2023. All Rights Reserved.", (0, 0, 0))
+    screen.blit(text_surface, (835, 700))
+    # or just `render_to` the target surface.
+    GAME_FONT.render_to(screen, (3, 700), "Beta V0.0.1", (0, 0, 0))
+
+    pygame.display.flip()
+
 while True:
   for event in pygame.event.get():
     if event.type == QUIT:
       pygame.quit()
       sys.exit()
   pygame.display.update()
- 
-#main
+  
+pygame.quit()
