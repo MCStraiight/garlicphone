4b6f42496d4319b299b2783cb32c9141ce97858e
StraightGG <ss2uang.good@gmail.com>
2023-10-20 20:30:12 +0900

    e

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 import sys
 from pygame.locals import *
 
-
 pygame.init()
 
 DISPLAYSURF = pygame.display.set_mode((1280, 720))
@@ -14,23 +13,27 @@ screen = pygame.display.set_mode((1280, 720))
 GAME_FONT = pygame.freetype.Font("fonts/ProximaNova.ttf", 24)
 running =  True
 
-logowt = pygame.image.load("images/logowt.png")
-logowt = pygame.transform.scale(logowt, (300, 800))
+clock = pygame.time.Clock()
+logowt = pygame.image.load("images/logowt.png").convert()
+# coordinates of the image
+x = 10;
+y = 20;
+screen.blit(logowt, (x, y))
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.blit(logowt, (100, 100))
     screen.fill((255,255,255))
-    # You can use `render` and then blit the text surface ...
-    text_surface, rect = GAME_FONT.render("Siwoo Company 2023. All Rights Reserved.", (0, 0, 0))
-    screen.blit(text_surface, (835, 700))
-    # or just `render_to` the target surface.
+    GAME_FONT.render_to(screen, (835, 700), "Siwoo Company 2023. All Rights Reserved.", (0, 0, 0))
     GAME_FONT.render_to(screen, (3, 700), "Beta V0.0.1", (0, 0, 0))
-
     pygame.display.flip()
+    
+    for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    run = False
 
 while True:
   for event in pygame.event.get():
@@ -40,3 +43,4 @@ while True:
   pygame.display.update()
   
 pygame.quit()
+quit()
